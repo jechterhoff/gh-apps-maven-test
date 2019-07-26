60e2bfdea276b71fe77dbb4c7589057bb4a48f74
Johannes Echterhoff <echterhoff@interactive-instruments.de>
2019-07-26 16:09:30 +0200

    Initial commit
    Signed-off-by: Johannes Echterhoff <echterhoff@interactive-instruments.de>

---
+++ src/main/java/org/example/GitHubAppsMavenTest/Main.java
@@ -0,0 +1,19 @@
+package org.example.GitHubAppsMavenTest;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+		if (args.length > 0 && StringUtils.isNotBlank(args[0])) {
+			System.out.println(args[0]);
+		} else {
+			System.out.println("Hello World!");
+		}
+	}
+}
